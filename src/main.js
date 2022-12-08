df86086a8277dc83420184e6e8294a77489a28f1
autumnly1007 <autumnly1007@gmail.com>
2022-12-08 14:48:23 +0900

    Add chart

--- src/main.js
+++ src/main.js
@@ -1,9 +1,52 @@
-async function getMovies(title) {
-  const res = await fetch('/api/movie', {
-    method: 'POST',
-    body: JSON.stringify({ title }),
+import * as echarts from 'echarts';
+
+(async () => {
+  async function getSales() {
+    const res = await fetch('/api/sales');
+    return await res.json();
+  }
+
+  const sales = await getSales();
+
+  const chartEl = document.querySelector('.chart');
+  const chart = echarts.init(chartEl);
+  chart.setOption({
+    tooltip: {
+      extraCssText: `
+      padding: 8px;
+      border-radius: 4px;
+      background-color: #333655;
+      color: #FFF;`,
+      formatter: (params) => {
+        const { name, value } = params;
+        return `
+          <div>Month: ${name}월</div>
+          <div>Sales: ${value.toLocaleString()}원</div>
+        `;
+      },
+    },
+
+    xAxis: {
+      data: Object.keys(sales),
+      axisLabel: {
+        formatter: (value) => `${value}월`,
+      },
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        type: 'bar',
+        data: Object.values(sales),
+        itemStyle: {
+          color: 'pink',
+        },
+      },
+      {
+        type: 'line',
+        data: Object.values(sales),
+      },
+    ],
   });
-  const json = await res.json();
-  console.log(json);
-}
-getMovies('frozen');
+})();
---
+++ src/movieMain.js
@@ -0,0 +1,9 @@
+async function getMovies(title) {
+  const res = await fetch('/api/movie', {
+    method: 'POST',
+    body: JSON.stringify({ title }),
+  });
+  const json = await res.json();
+  console.log(json);
+}
+getMovies('frozen');
